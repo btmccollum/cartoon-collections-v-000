e356af7e4dd2d005daedb3ac00df6d61288d4aed
Brad McCollum <btmcco8@gmail.com>
2018-08-13 19:07:59 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(dwarf_names) #takes an array of names and puts out a numbered list of names
-  dwarf_names.each_with_index{|name, index| puts "#{index.to_i + 1}. #{name}"}
+  dwarf_names.each_with_index {|name, index| puts "#{index.to_i + 1}. #{name}"}
 end
 
 def summon_captain_planet(planeteer_calls) #takes in an array of planeteer calls, capitalizes their names, adds an !, and returns a new array
