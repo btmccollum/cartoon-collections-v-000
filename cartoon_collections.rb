c6bf5338b6d5f35c78c5723704081d56763e9ccd
Brad McCollum <btmcco8@gmail.com>
2018-08-13 19:01:40 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,8 +1,8 @@
-def roll_call_dwarves(array) # code an argument here
+def roll_call_dwarves(dwarf_names) # code an argument here
   array.each_with_index{|name, index| puts "#{index.to_i + 1}. #{name}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
 end
 
