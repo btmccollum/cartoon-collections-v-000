54a5cf99c274ea5b2ba52cf964fe8e9d90f91090
Brad McCollum <btmcco8@gmail.com>
2018-08-13 19:12:37 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,13 @@ def long_planeteer_calls(long_planeteer_calls)
   long_planeteer_calls.any? {|call| call.length > 4 }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(snacks)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  snacks.include?(cheese_types)
+    if snack == cheese_types[snack]
+
+    else
+      nil
+    end
 end
